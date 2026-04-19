8e88f6ee6338915f2a749c6d1a5c1efdd8aa6948
nightstudd <artemolhovskiy0904@gmail.com>
2026-04-19 21:44:43 +0200

    created full registration

--- register.py
+++ register.py
@@ -1,5 +1,7 @@
 import time
 import json
+import os
+from colorama import Fore, Style
 
 print("Loading data.")
 
@@ -10,14 +12,54 @@ def save_data():
     with open("database.json", "w")as d:
         json.dump(data, d, indent=4 )
 
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 time.sleep(2)
 print("loadded")
 time.sleep(2)
+while True:
+    if data['login'] == "":
+        clear_console()
+        print("You need register.")
+        time.sleep(2.1)
+        create_login = input("Write a login here : ")
+        data['login'] =  create_login
+        save_data()
+        print(f"Your login : {data['login']} ")
 
-if data['login'] == "":
-    print("You need register.")
-    time.sleep(2.1)
-    create_login = input("Write a login here : ")
-    data['login'] =  create_login
-    save_data()
-    print(f"Your login : {data['login']} ")
+    if data['password']  == "":
+        clear_console()
+        print(Fore.GREEN + "Time a create password!" + Style.RESET_ALL)
+        create_password = input("Write your password : ")
+        data['password'] = create_password
+        save_data()
+        time.sleep(1)
+        print("Success")
+
+    clear_console()
+    print("Is information correct?")
+    print("##########")
+    print(f" Your login : {data['login']}")
+    print(Fore.RED + f" Your password : {data['password']}" + Style.RESET_ALL)
+    print("###########")
+    print(Fore.RED + "DONT SHOW YOUR PASSWORD TO ANYONE" + Style.RESET_ALL)
+    time.sleep(3)
+
+    choice = input("yes or no  : ").strip().lower()
+
+    if choice == "yes":
+        break
+
+    elif choice == "no":
+
+        data['login'] = ""
+        data['password'] = ""
+        save_data()
+        continue
+
+
+while True:
+    print(f"Welcome {data['login']}")
+    break # === delete this if you dont need.
+
